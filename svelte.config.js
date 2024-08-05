ce4be9803828ac41366e3103e07008887f0f800d
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-08-05 01:38:53 -0500

    [MODIFY] Changing the deployment branch to main

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
             strict: true
         }),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+            base: process.env.NODE_ENV === 'production' ? '' : '',
         }
     }
 };
