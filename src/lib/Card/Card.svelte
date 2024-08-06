713c04a1699601bd5ba3c70fc9702a85c1c6ad7d
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-08-05 23:31:08 -0500

    [ADD] Adding the logos to the project route

---
+++ src/lib/Card/Card.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let title;
+    export let subtitle;
+</script>
+
+<style>
+    .card {
+        border-radius: 8px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        background-color: #fff;
+        margin: 8px;
+        width: 160px;
+        height: 160px;
+        cursor: pointer;
+    }
+
+    h2 {
+        margin: 0;
+    }
+
+    p {
+        color: #666;
+    }
+</style>
+
+<div class="card p-8 flex justify-center align-end">
+    <h2>{title}</h2>
+    <p>{subtitle}</p>
+    <slot></slot>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,11 @@
 <script>
+	import Card from '$lib/Card/Card.svelte';
     import Carousel from 'svelte-light-carousel';
-    const slides = Array.from({ length: 10 }, (_, i) => ({ title: `${i + 1}` }));
+    const slides = ["/android_logo.png", "/ios_logo.png", "/flutter_logo.png", "/ionic_logo.png"]
+    
 </script>
 
+<p class="text-2xl font-bold">Mobile Development</p>
 <div class="flex width-full justify-center">
     <Carousel {slides} 
               autoPlay={2}
@@ -13,7 +16,20 @@
                 lg: 3,
                 xl: 4
             }}
+            slideClass="cp_carousel"
               >
-        <div slot="slide" let:slide >{slide.title}</div>
+        <div  class="flex justify-center" slot="slide" let:slide >
+            <a href={slide}>
+                <Card title="" subtitle=""> 
+                    <img class="logo_icon self self-end" src={slide} alt="logo"/>
+                </Card>
+            </a>    
     </Carousel>
-</div>    
+</div>
+
+<style lang="postcss">
+    .logo_icon {
+        @apply w-20 h-auto;
+    }
+   
+</style>
