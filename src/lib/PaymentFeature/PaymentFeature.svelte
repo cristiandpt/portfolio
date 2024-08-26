04b93b498feaed19b81dada6ba309d20a8b8fef7
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-08-25 22:46:19 -0500

    Componentizing the Payment feature

---
+++ src/lib/PaymentFeature/PaymentFeature.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+	import '@aarsteinmedia/dotlottie-player';  
+</script>
+
+<div class="flex gap-16 mt-8">
+    <dotlottie-player
+      class=""
+      autoplay=""
+      subframe=""
+      loop=""
+      src="{base}/payment_anim.json"
+      style="width: 480px; height: 380px; margin-block-end: 12px;"
+    > 
+    </dotlottie-player>
+    <div class="flex flex-col align-center justify-center">
+      <h2 class="text-3xl font-bold">Pasarela de pagos</h2>
+      <p class="h-auto py-4 w-[400px]">Implementación del SDK de epayco y Payu para el pago electronico de los diferentes servicios ofrecidos por la app, además del recarga de la billetera virtual.</p>
+    </div>
+  </div>
--- src/routes/projects/android/+page.svelte
+++ src/routes/projects/android/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 
   import { base } from '$app/paths';
+	import PaymentFeature from '$lib/PaymentFeature/PaymentFeature.svelte';
 	import { Images } from "$lib/svelte-images";
   import '@aarsteinmedia/dotlottie-player';  
 
@@ -108,21 +109,7 @@
    </dotlottie-player> 
   </div>
   
-  <div class="flex gap-16 mt-8">
-      <dotlottie-player
-        class=""
-        autoplay=""
-        subframe=""
-        loop=""
-        src="{base}/payment_anim.json"
-        style="width: 480px; height: 380px; margin-block-end: 12px;"
-      > 
-      </dotlottie-player>
-      <div class="flex flex-col align-center justify-center">
-        <h2 class="text-3xl font-bold">Pasarela de pagos</h2>
-        <p class="h-auto py-4 w-[400px]">Implementación del SDK de epayco y Payu para el pago electronico de los diferentes servicios ofrecidos por la app, además del recarga de la billetera virtual.</p>
-      </div>
-    </div>
+  <PaymentFeature />
 
     <div class="flex gap-16 mt-16">
       <div class="flex flex-col align-center justify-center">
