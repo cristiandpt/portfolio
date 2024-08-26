e2a51dfaf51dfe5a0e86057204d884770c296c14
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-08-25 22:22:42 -0500

    Adding the firebase authentication description

--- src/routes/projects/android/+page.svelte
+++ src/routes/projects/android/+page.svelte
@@ -147,7 +147,7 @@
     <div class="flex gap-16 mt-32">
       <div class="flex flex-col align-center justify-center">
         <h2 class="text-3xl font-bold">Autenticación a través <br/> servicios de Firebase</h2>
-        <p class="h-auto py-4 w-[400px]">Separación de responsabilidades entre la vista; el controlador, que despacha eventos a capas profundas y expone data a subscribir en la vista; y el modelo, que ejecuta la lógica de negocio.</p>
+        <p class="h-auto py-4 w-[400px]">Implementación de la autenticación de Firebase para el registro y acceso de usuarios a la app mediante proveedores de autenticación como Google, Facebook y Twitter.</p>
       </div>
       <div class="flex flex-row align-center justify-center gap-4 ps-16"> 
         <img src="{base}/firebase_services.svg" 
