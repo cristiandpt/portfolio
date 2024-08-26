faadd6aafef918d34efa83c7b7e52d4dd82b0d60
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-08-26 17:46:21 -0500

    Adding the sun page

--- src/routes/projects/vidriossol/+page.svelte
+++ src/routes/projects/vidriossol/+page.svelte
@@ -7,33 +7,48 @@
 
     const images = [
       {
-        src: `${base}/main_flutter_blured.png`
+        src: `${base}/elsol/landing_page_sun_blur.png`
       },
       {
-        src: `${base}/Result_measurement_flutter.png`
+        src: `${base}/elsol/sun_in_blur.png`
       },
       {
-        src: `${base}/eclinic_presentation.png`
+        src: `${base}/elsol/sun_frames_custom_blur.png`
+      }, 
+      {
+        src: `${base}/elsol/checkout_blur.png`
+      },
+       {
+        src: `${base}/elsol/frame_landscape_blur.png`
       },
       {
-        src: `${base}/kiosk_f.png`
+        src: `${base}/elsol/paspartu_blur.png`
       },
       {
-        src: `${base}/flutter_cali.png`
+        src: `${base}/elsol/landscape.png`
       },
       {
-        src: `${base}/accountt.png`
+        src: `${base}/elsol/mirror_frame_blur.png`
       },
       {
-        src: `${base}/specialitites.png`
-    },
-    {
-        src: `${base}/report_android.png`}
+        src: `${base}/elsol/customoize_frame_blur.png`
+      }, 
+      {
+        src: `${base}/elsol/articles.png`
+      },
+     
+      
     ];
 
 </script>
 
+<style>
+  .gallery-container {
+    height: 75vh;
+  }
+</style>
 
+<h2 class="mb-8">Gallery</h2>
 <div class="p-4 gallery-container mb-16 flex justify-center content-center" >
-  <Images {images} numCols={5} />
+  <Images {images} numCols={2} />
 </div>
