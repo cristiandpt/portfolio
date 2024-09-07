f5689c5ed040bc1a27330b36f261e10f046b5775
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-09-06 23:00:11 -0500

    Adding the about me traslation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
     import { base } from '$app/paths';
+    import { t } from 'svelte-i18n';
 </script>
 
-<h1 class="text-3xl text-gray-800 font-bold text-center mb-12">About Me</h1>
-<p class="text-lg text-gray-600 text-center w-1/2 mx-auto ">I'm an Cristian David Pacheco, an autodidact with a passion for technology and a curiosity that drives me to experiment with new things. I love diving into the latest tech trends and figuring out how they work. My hobbies include savoring delicious food, traveling to new places, and immersing myself in books, especially those that delve into philosophy. Exploring different philosophies helps me broaden my perspective and understand the world in unique ways.</p>
+<h1 class="text-3xl text-gray-800 font-bold text-center mb-12">{$t('about_me.title', { default: 'About' })}</h1>
+<p class="text-lg text-gray-600 text-center w-1/2 mx-auto ">{$t('about_me.description', { default: 'Descripcion' })}</p>
 <div class="mt-4 w-[240px] h-[240px] mx-auto">
     <DotLottieSvelte
     src="{base}/think.json"
